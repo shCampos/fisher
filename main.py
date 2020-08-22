41a4543fcf7c4536b3e2de872bb4312f48f84aaa
shcampos <samuel-hc@hotmail.com>
2020-08-22 15:10:27 -0400

    feat: basic statistics equations | freq=1

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
+import numpy as np
+import statistics as st
 
-array = list(map(float, input('Digite os números:\n').split()))
-print(array)
+dados = list(map(float, input('Digite os números:\n').split()))
 
-print(math.ceil(array[0]))
+print("Media: ", np.mean(dados))
+print("Mediana: ", np.median(dados))
+print("Moda: ", st.mode(dados))
+print("Amplitude: ", np.ptp(dados))
+print("Desvio padrão: ", np.std(dados))
+print("Variancia: ", np.var(dados))
