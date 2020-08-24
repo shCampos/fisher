2585f58081115f9ded7a30619b9806a17507b24c
shcampos <samuel-hc@hotmail.com>
2020-08-23 21:09:38 -0400

    feat: basic statistics equations | freq != 1

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import numpy as np
 import statistics as st
 
-dados = list(map(float, input('Digite os números:\n').split()))
+def tratarDados(d):
+    dSeparado = d.split(',')
+    if len(dSeparado)>1:
+        return [float(dSeparado[0]), int(dSeparado[1])]
+    else:
+        return [float(dSeparado[0]), 1]
 
-print("Media: ", np.mean(dados))
+dados = list(map(tratarDados, input('Digite os números:\n').split()))
+print(dados)
+
+print("Media: ", np.average(dados))
 print("Mediana: ", np.median(dados))
-print("Moda: ", st.mode(dados))
+#print("Moda: ", st.mode(dados))
 print("Amplitude: ", np.ptp(dados))
 print("Desvio padrão: ", np.std(dados))
 print("Variancia: ", np.var(dados))
